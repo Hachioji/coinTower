d727238bd85fe28dfa9a4d0a3ef8646d722aa2b0
ralsoon <smallkirby423@yahoo.co.jp>
2015-09-21 19:16:08 +0900

    これはtestじゃよ
    by テストおじさん

---
+++ CoinTower/Assets/Test.cs
@@ -0,0 +1,54 @@
+﻿// ----- ----- ----- ----- -----
+//
+// Test
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("MyScript/Test")]
+public class Test : MonoBehaviour
+{
+	#region 変数
+
+
+
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+
+    }
+
+    // 更新前処理
+    void Start()
+    {
+
+    }
+
+    // 更新処理
+    void Update()
+    {
+
+    }
+	#endregion
+}
