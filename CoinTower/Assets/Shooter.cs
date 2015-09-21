1e82425d95bff703613be29aa65778d6c43c02ec
ralsoon <smallkirby423@yahoo.co.jp>
2015-09-21 22:25:50 +0900

    shooter coin など追加

---
+++ CoinTower/Assets/Shooter.cs
@@ -0,0 +1,58 @@
+﻿// ----- ----- ----- ----- -----
+//
+// Shooter
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Coin/Shooter")]
+public class Shooter : MonoBehaviour
+{
+	#region 変数
+
+    [SerializeField]
+    private GameObject coin;
+
+    #endregion
+
+
+    #region プロパティ
+
+
+
+    #endregion
+
+
+    #region メソッド
+
+	// 初期化処理
+    void Awake()
+    {
+
+    }
+
+    // 更新前処理
+    void Start()
+    {
+
+    }
+
+    // 更新処理
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(coin,transform.position,Quaternion.identity);
+        }
+    }
+	#endregion
+}
